c1b95ba5544c08c130b0a58183ed64b0ec89ccb7
Robert Rajakone <robert.rajakone@fhnw.ch>
2020-02-15 19:20:50 +0100

    Revert "Datasource Adapter: VisualizerApi can download fields from datapoint collection"
    This reverts commit f7e1c2d4

--- mlpipe/test/datasources/test_visualizer_api_adapter.py
+++ mlpipe/test/datasources/test_visualizer_api_adapter.py
@@ -6,11 +6,10 @@ class TestVisualizerApiAdapter(unittest.TestCase):
     def test_standard_case(self):
         data = VisualizerApiAdapter(
             fields=["40210033 as CO2", "40210032 as InnenTemperatur"],
-            data_collection="f2179b46-bfb4-4e49-8f7b-bf2bd03d01c1",
             username='NEST\\raro',
             password='W3lc0me!2018$',
-            date_from='2019-02-15T12:00',
-            date_to='2019-02-15T13:00').get()
+            date_from='2020-02-15T12:00',
+            date_to='2020-02-15T13:00').get()
 
         self.assertIsNotNone(data.timestamps)
         self.assertIsNotNone(data.data)
