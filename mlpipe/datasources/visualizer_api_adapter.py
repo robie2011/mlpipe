2833baf5ca0ea5acd42c7e0bf128d988a8c8953c
Robert Rajakone <robert.rajakone@fhnw.ch>
2020-02-15 21:07:24 +0100

    Revert "Datasource Adapter: VisualizerApi can download fields from datapoint collection"
    This reverts commit f7e1c2d4

--- mlpipe/datasources/visualizer_api_adapter.py
+++ mlpipe/datasources/visualizer_api_adapter.py
@@ -1,53 +1,35 @@
+from dataclasses import dataclass
+from typing import List, Union
+import numpy as np
+from mlpipe.datasources.abstract_datasource_adapter import AbstractDatasourceAdapter, Field
+from mlpipe.datasources.empa import EmpaCsvSourceAdapter
+from mlpipe.processors.standard_data_format import StandardDataFormat
 import datetime
 import urllib
-from typing import List
-import pandas as pd
 import requests
 from requests_ntlm import HttpNtlmAuth
-from mlpipe.datasources.abstract_datasource_adapter import AbstractDatasourceAdapter, Field
-from mlpipe.processors.standard_data_format import StandardDataFormat
+import pandas as pd
 
 
 class VisualizerApiAdapter(AbstractDatasourceAdapter):
-    def __init__(self,
+    def __init__(self, fields: List[str],
                  username: str,
                  password: str,
                  date_from: str,
-                 date_to: str,
-                 fields: List[str] = [],
-                 data_collection: str = None):
-        if not data_collection and fields:
-            raise ValueError("Property required: fields or data_collection")
+                 date_to: str):
         super().__init__(fields=fields)
-
         self.session = requests.Session()
         self.session.auth = HttpNtlmAuth(username=username, password=password)
         self.date_from = datetime.datetime.fromisoformat(date_from)
         self.date_to = datetime.datetime.fromisoformat(date_to)
 
-        if data_collection:
-            for name in self._get_collection_points(session=self.session, name=data_collection):
-                if name in self.fields:
-                    continue
-                self.logger.info(f"adding field from datapoint collection: {name}")
-                self.fields.append(name)
-
     def _fetch(self, _fields: List[Field]) -> StandardDataFormat:
-        delta = (self.date_to - self.date_from)
-        self.logger.info(
-            f"Requested timewindow for download is from {self.date_from} to {self.date_to}. Window size is: {delta}")
         series = []
         for f in _fields:
             self.logger.info(f"getting data for field: {f.name} aka. {f.alias}")
             url = self._get_url(sensor_id=f.name, date_from=self.date_from, date_to=self.date_to)
             self.logger.debug(f"download data from: {url}")
             resp = self.session.get(url)
-            if resp.status_code != 200:
-                raise Exception(resp.text)
-
-            if resp.text.strip() == "[]":
-                msg = f"No measurements found for sensor: {f.name} in given time period: {self.date_from} to {self.date_to}."
-                raise Exception(msg)
 
             serie = pd.read_json(resp.text)
             serie = serie.set_index('timestamp')
@@ -76,8 +58,3 @@ class VisualizerApiAdapter(AbstractDatasourceAdapter):
 
         return url + urllib.parse.urlencode(params)
 
-    @staticmethod
-    def _get_collection_points(session: requests.session, name: str):
-        url = f"https://visualizer.nestcollaboration.ch/Backend/api/v1/datapointcollections/{name}"
-        resp = session.get(url)
-        return resp.json()['items']
