c55c28a7531ca956e837b829859fdb6b2b883f10
Robert Rajakone <robert.rajakone@fhnw.ch>
2020-02-20 13:57:01 +0100

    no issue: info seed setup

--- mlpipe/processors/seed_setup.py
+++ mlpipe/processors/seed_setup.py
@@ -7,16 +7,20 @@ class SeedSetup(AbstractProcessor):
         import random
         self.np_seed = numpy or random.randint(0, 1000)
         self.tf_seed = tensorflow or random.randint(0, 1000)
+        self.logger.info(f"using np_seed {self.np_seed}")
+        self.logger.info(f"using tf_seed {self.tf_seed}")
 
     def _process2d(self, processor_input: StandardDataFormat) -> StandardDataFormat:
         # only import if instance is created
         import tensorflow
         import numpy
 
+        # results are not always reproduciable
         # https://machinelearningmastery.com/reproducible-results-neural-networks-keras/
         # https://www.tensorflow.org/api_docs/python/tf/random/set_seed?version=stable
         self.get_logger().info("using numpy random seed={0}".format(self.np_seed))
         self.get_logger().info("using tensorflow random seed={0}".format(self.tf_seed))
         numpy.random.seed(self.np_seed)
+
         tensorflow.random.set_seed(self.tf_seed)
         return processor_input
