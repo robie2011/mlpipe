9a84fa86ec6edd697ac835d34f3404c4a1183496
Robert Rajakone <robert.rajakone@fhnw.ch>
2020-02-26 09:24:12 +0100

    build & deploy stuff

--- install_environment.sh
+++ install_environment.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-#tar -xvf mlpipe.gz.tar
 apt-get update && apt-get install vim -y
 pip install dataclasses
 pip install -r requirements.txt
 
-
-sed -i -e 's/from __future__ import annotations//g' mlpipe/processors/standard_data_format.py
-sed -i -e 's/from __future__ import annotations//g' mlpipe/dsl_interpreter/config_reader.py
--- vast_create.sh
+++ vast_create.sh
@@ -1,4 +1,5 @@
-vast create instance $1 --image tensorflow/tensorflow:2.0.0b1-gpu-py3-jupyter --disk 5
+#vast create instance $1 --image tensorflow/tensorflow:2.0.0b1-gpu-py3-jupyter --disk 5
+vast create instance $1 --image robie2011/mlpipe --disk 5
 
 STATUS=$(vast show instances | tail -n1 | tr -s " " | cut -d' ' -f3)
 
@@ -6,7 +7,7 @@ while [ "$STATUS" != "running" ]
 do
   sleep 5s
   STATUS=$(vast show instances | tail -n1 | tr -s " " | cut -d' ' -f3)
-  echo "check $(date) ..."
+  echo "check $(date): $STATUS ..."
 done
 
 echo "container is running!"
