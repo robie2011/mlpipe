ea7a63b4dddb051393294582505e434397fea8be
Robert Rajakone <robert.rajakone@fhnw.ch>
2020-03-05 23:22:56 +0100

    comments and variable renaming

--- mlpipe/processors/freezed_value_remover.py
+++ mlpipe/processors/freezed_value_remover.py
@@ -21,28 +21,58 @@ def _flatten_list(data):
     return list(itertools.chain(*data))
 
 
-def get_mask_for_freezed_values(data: np.ndarray, threshold: int):
-    match = data[1:] == data[:-1]
-    return _get_mask(data.shape, threshold=threshold, match=match)
+def _get_mask(data: np.ndarray, threshold: int):
 
-
-def _get_mask(data_shape: Tuple[int, ...], threshold: int, match: np.ndarray):
     """
-    Input: 2D matrix having different series on each column.
-    detecting unchanged values for more than `threshold`-times
-    return: 2D matrix with boolean value representing invalid (TRUE) values
+    Algorithm is explained by exampel below:
+
+        Given is 1D-Variable called value.
+        We create an array called next_equal which gives TRUE if next value is equal.
+        We create an array called freeze_change which represents points on which
+            freezed value has started or stopped.
+        By getting only TRUE values from freeze_change-array we have all change points (array ix_change_col).
+        Every 2nd point of ix_change_col represents starting point of freezed value.
+        Every 2nd point starting from index=1 represents end point of freezd value.
+        Now we can calculate difference between start and endpoint and handle difference more than threshold.
+
+
+    Note:
+        - freez_change on table below needs offset: -1
+        - (d) is a dummy value on top and bottom of array
+
+    | Index | Value | next_equal | freeze_change |
+    |-------|-------|------------|---------------|
+    |     0 |    20 | FALSE (d)  |               |
+    |     1 |    21 | FALSE      | FALSE         |
+    |     2 |    22 | FALSE      | FALSE         |
+    |     3 |    20 | FALSE      | FALSE         |
+    |     4 |    20 | TRUE       | TRUE          |
+    |     5 |    20 | TRUE       | FALSE         |
+    |     6 |    20 | TRUE       | FALSE         |
+    |     7 |    20 | TRUE       | FALSE         |
+    |     8 |    22 | FALSE      | TRUE          |
+    |     9 |    23 | FALSE      | FALSE         |
+    |       |       | FALSE (d)  | FALSE         |
+
+
     """
 
-    n_rows, n_cols = data_shape
-    data_mask = np.zeros(data_shape, dtype='bool')
+    n_rows, n_cols = data.shape
+    data_mask = np.zeros(data.shape, dtype='bool')
 
+    # next_equal: boolean shows whether following measurement has same value
+    # dummy data on top and bottom of match-array for counting freed values
     dummy_data = [[False] * n_cols]
-    mask = np.r_[dummy_data, match, dummy_data]
-    has_diff_to_previous: np.ndarray = mask[1:] != mask[:-1]
-    indexes = np.arange(has_diff_to_previous.shape[0])
+    next_equal = np.r_[
+        dummy_data,
+        data[1:] == data[:-1],
+        dummy_data]
+
+    freeze_change = next_equal[1:] != next_equal[:-1]
+    indexes = np.arange(freeze_change.shape[0])
 
     for col in range(n_cols):
-        ix_change_col = indexes[has_diff_to_previous[:, col]]
+        ix_change_col = indexes[freeze_change[:, col]]
         ix_start = ix_change_col[::2]
         ix_end = ix_change_col[1::2]
         ranges = _get_range_exceeding_maximum_length(ix_start, ix_end, threshold)
@@ -56,7 +86,7 @@ class FreezedValueRemover(AbstractProcessor):
         self.max_freezed_values = max_freezed_values
 
     def _process2d(self, processor_input: StandardDataFormat) -> StandardDataFormat:
-        mask = get_mask_for_freezed_values(processor_input.data, self.max_freezed_values)
+        mask = _get_mask(processor_input.data, self.max_freezed_values)
         data = processor_input.data.copy()
         data[mask] = np.nan
         return processor_input.modify_copy(data=data)
