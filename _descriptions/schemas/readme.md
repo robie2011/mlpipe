b05ff7e3c00aa47f42da609e0ceef4eba5a838ef
Robert Rajakone <robert.rajakone@fhnw.ch>
2020-03-16 15:01:50 +0100

    moving schemas, cleaning vast, test data

--- _descriptions/schemas/readme.md
+++
@@ -1,9 +0,0 @@
-
-```python
-f = open("_descriptions/schemas/pipe.schema.json")
-jsonref.load(f, base_uri="file:///Users/robert.rajakone/repos/2019_p9/code/_descriptions/schemas/", jsonschema=True)
-```
-
-
-# Visualizer
-https://navneethg.github.io/jsonschemaviewer/
--- mlpipe/dsl/sources.py
+++ mlpipe/dsl/sources.py
@@ -1,6 +1,4 @@
 # noinspection PyUnresolvedReferences
-from mlpipe.datasources.demo_live_data import DemoLiveData
-# noinspection PyUnresolvedReferences
 from mlpipe.datasources.empa_csv_source_adapter import EmpaCsvSourceAdapter
 # noinspection PyUnresolvedReferences
 from mlpipe.datasources.visualizer_api_adapter import VisualizerApiAdapter
